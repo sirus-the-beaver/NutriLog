6fb5143ade63a9dd62cb9314821a20dbfa9b0284
Sirus Salari <salaris@oregonstate.edu>
2024-08-10 19:48:58 -0700

    Configure tailwind

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    'module:@react-native/babel-preset',
+    'module:metro-react-native-babel-preset',
+  ],
+  plugins: ['nativewind/babel'],
 };
