ecc19364a3b69917d0c9a79817291dc3046bb407
Sirus Salari <salaris@oregonstate.edu>
2024-08-16 21:29:40 -0700

    Add bottom tab navigation

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import AppNavigator from './src/navigation/AppNavigator';
-import SignOut from './src/components/SignOut';
 
 const App = () => (
     <AppNavigator />
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SignUp from '../components/SignUp';
 import SignIn from '../components/SignIn';
 import SignOut from '../components/SignOut';
 import FoodLogForm from '../components/FoodLogForm';
 import FoodLogList from '../components/FoodLogList';
 import BarcodeScanner from '../components/BarcodeScanner';
+import ExerciseLogForm from '../components/ExerciseLogForm';
+import ExerciseLogList from '../components/ExerciseLogList';
+import ProgressForm from '../components/ProgressForm';
+import ProgressList from '../components/ProgressList';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
     return (
@@ -20,7 +26,17 @@ const AppNavigator = () => {
                 <Stack.Screen name="FoodLogForm" component={FoodLogForm} />
                 <Stack.Screen name="FoodLogList" component={FoodLogList} />
                 <Stack.Screen name="BarcodeScanner" component={BarcodeScanner} />
+                <Stack.Screen name="ExerciseLogForm" component={ExerciseLogForm} />
+                <Stack.Screen name="ExerciseLogList" component={ExerciseLogList} />
+                <Stack.Screen name="ProgressForm" component={ProgressForm} />
+                <Stack.Screen name="ProgressList" component={ProgressList} />
             </Stack.Navigator>
+
+            <Tab.Navigator>
+                <Tab.Screen name="FoodLogList" component={FoodLogList} />
+                <Tab.Screen name="ExerciseLogList" component={ExerciseLogList} />
+                <Tab.Screen name="ProgressList" component={ProgressList} />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 }
