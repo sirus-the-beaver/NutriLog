b39cc3a6ce801f16092595711681b054f8dd9864
Sirus Salari <salaris@oregonstate.edu>
2024-08-17 17:05:04 -0700

    Create rate limiting middleware

---
+++ backend/middleware/rateLimit.js
@@ -0,0 +1,10 @@
+const rateLimit = require('express-rate-limit');
+
+// Rate limit middleware
+const rateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: 'Too many requests, please try again later',
+});
+
+module.exports = rateLimiter;
