362f45189e9d4607930093c96ca6af1c7110751a
Sirus Salari <salaris@oregonstate.edu>
2024-08-17 17:06:42 -0700

    Apply rate limiting middleware to all api routes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const auth = require('./middleware/auth');
+const rateLimit = require('./middleware/rateLimit');
 const userRoutes = require('./routes/userRoutes');
 const purchaseRoutes = require('./routes/purchaseRoutes');
 const subscriptionRoutes = require('./routes/subscriptionRoutes');
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(rateLimit);
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
