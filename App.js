4caa95b134587e1a7d5ff98a5e2841b3385bd59a
Sirus Salari <salaris@oregonstate.edu>
2024-08-27 22:11:09 -0700

    Configure react-native-google-mobile-ads and implement in app

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import 'react-native-gesture-handler';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { REVENUECAT_API_KEY, REVENUECAT_DEBUG } from './src/utils/config';
 import Purchases from 'react-native-purchases';
+import { Platform, View } from 'react-native';
+import mobileAds, { BannerAd, TestIds, BannerAdSize, useForeground } from 'react-native-google-mobile-ads';
+
+mobileAds()
+    .initialize()
+    .then(() => {
+        console.log('Google Mobile Ads SDK initialized');
+    });
+
+const ANDROID_BANNER_AD_UNIT_ID = 'ca-app-pub-3405415006338202/5867715094';
+const IOS_BANNER_AD_UNIT_ID = 'ca-app-pub-3405415006338202/8651523045';
+
+const bannerAdUnitId = Platform.select({
+    ios: IOS_BANNER_AD_UNIT_ID,
+    android: ANDROID_BANNER_AD_UNIT_ID,
+});
+
+const adUnitId = __DEV__ ? TestIds.ADAPTIVE_BANNER : bannerAdUnitId;
 
 const App = () => {
     useEffect(() => {
@@ -12,8 +30,23 @@ const App = () => {
         })
     }, []);
 
+    const bannerRef = useRef<BannerAd>(null);
+
+    useForeground(() => {
+        Platform.OS === 'ios' && bannerRef.current?.load();
+    });
+
     return (
-        <AppNavigator />
+        <View style={{ flex: 1 }}>
+            <BannerAd
+                unitId={adUnitId}
+                size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+                requestOptions={{
+                    requestNonPersonalizedAdsOnly: true,
+                }}
+            />
+            <AppNavigator />
+        </View>
     )
 };
 
